60ef133f40700d1a5dfaa8a2784da52bcd5d43e1
prasann-28 <prasann.shimpi@icloud.com>
2022-02-01 15:21:51 +0530

    helper tests

--- Code/helper_test.py
+++ Code/helper_test.py
@@ -1,13 +1,16 @@
+# Imports helper.py for testing purposes
 from helper import *
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from keras.models import load_model
 
-
 message = 'hello world'
+
+# Block padding and size to be kept constant
 block_padding= 11
 block_size = 16
 
+# Encryption of message and key generation and binary conversion
 encrypt = encstr(message,block_padding)
 print(encrypt)
 bin_ciph = strToArr(encrypt[0], block_size)
@@ -17,11 +20,13 @@ alice = load_model('alice.h5')
 bin_message = bin_ciph[0]
 bin_key = bin_ciph[1]
 
+# Alice's prediction
 cipher = alice.predict([bin_message, bin_key])
 
 bob  = load_model('bob.h5')
 eve = load_model('eve.h5')
 
+# Bob and Eve decryption test
 bob_pred = (bob.predict([cipher, bin_key]) > 0.5).astype(int)
 eve_pred = (eve.predict(cipher) > 0.5).astype(int)
 
